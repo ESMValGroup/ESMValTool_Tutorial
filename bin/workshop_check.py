c1a6edd68298f52ad14b3b2128e555418fc02dca
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2020-07-17 12:24:04 +0200

    add escience academy to checks

--- bin/workshop_check.py
+++ bin/workshop_check.py
@@ -388,7 +388,7 @@ def check_config(reporter, filename):
                    kind)
 
     carpentry = config.get('carpentry', None)
-    reporter.check(carpentry in ('swc', 'dc', 'lc', 'cp'),
+    reporter.check(carpentry in ('swc', 'dc', 'lc', 'cp', 'ea'),
                    filename,
                    'Missing or unknown carpentry: {0}',
                    carpentry)
